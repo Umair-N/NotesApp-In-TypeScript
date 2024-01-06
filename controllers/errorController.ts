a0c773396c9d2a9ff52c91da6f8c16e3b475dbd2
Sheikh <umairnajeeb95@gmail.com>
2024-01-06 17:56:01 +0530

    Enhanced Error Handling

--- controllers/errorController.ts
+++ controllers/errorController.ts
@@ -3,21 +3,32 @@ import AppError from "../utils/appError";
 
 const globalErrorHandler = (
   error: AppError,
-  req: Request,
+  _req: Request,
   res: Response,
-  next: NextFunction
+  _next: NextFunction
 ) => {
   // Extract status and statusCode from the error, default to "Error" and 400 if not present
   error.status = error.status || "Error";
   error.statusCode = error.statusCode || 400;
 
-  // Send a JSON response with the error details and status code
-  res.status(error.statusCode).json({
-    status: error.status,
-    message: error.message,
-    ErrorStack: error.stack,
-    error,
-  });
+  if (process.env.NODE_ENV === "development") {
+    // Send a JSON response with the error details and status code
+    res.status(error.statusCode).json({
+      status: error.status,
+      message: error.message,
+      ErrorStack: error.stack,
+      error,
+    });
+  } else if (process.env.NODE_ENV === "production") {
+    if (error.isOperational === true) {
+      res.status(error.statusCode).json({
+        status: error.status,
+        message: error.message,
+      });
+    }else{
+        res.status(500).json({status : 'Error', message : "Something went wrong please try again later "})
+    }
+  }
 };
 
-export default globalErrorHandler
+export default globalErrorHandler;
